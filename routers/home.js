21ccaea2cd50e44fed521e7155a0062771da2be7
jmeno1011 <jwhltn8282@gmail.com>
2022-04-11 21:50:03 +0900

    test 용 url만듬

--- routers/home.js
+++ routers/home.js
@@ -1,6 +1,17 @@
 const express = require("express");
+const db = require("../db/db");
 const router = express.Router();
 
 router.get("/", (req, res) => {
   res.send("<h1>home 입니다.</h1>");
+});
+
+router.get("/test", (req, res) => {
+  const userTable = "select * from userTable";
+  db.query(userTable, (err, result) => {
+    if (err) {
+      console.log(`Error발생 시간::${new Date()}::${err}`);
+    }
+    res.status(200).json({ table: result });
+  });
 });
