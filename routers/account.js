b00eb23db569b965726decde1a806d231640bb10
jmeno1011 <jwhltn8282@gmail.com>
2022-04-11 23:46:41 +0900

    따옴표 빠짐

--- routers/account.js
+++ routers/account.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.post("/login", (req, res) => {
   const { user_id, user_pw } = req.body;
-  const userId = `select user_id from userTable where user_id=${user_id}`;
+  const userId = `select user_id from userTable where user_id='${user_id}'`;
   db.query(userId, (err, result) => {
     if (err) {
       console.log(`Error발생 시간::${new Date()}::${err}`);
