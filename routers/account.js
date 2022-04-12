9742b8f7dcbbc10a521b97fad2be42f147bff362
meno1011 <39692905+meno1011@users.noreply.github.com>
2022-04-12 18:14:26 +0900

    login에 json값 변경

--- routers/account.js
+++ routers/account.js
@@ -1,19 +1,22 @@
 const express = require("express");
 const db = require("../db/db");
+const { generateAccessToken, generateRefreshToken } = require("../jwt/jwt");
 const router = express.Router();
 // const bcrypt = require("bcrypt");
 
 router.post("/login", (req, res) => {
   const { user_id, user_pw } = req.body;
-  console.log("user_id::",user_id);
-  console.log("user_pw::",user_pw);
+  console.log("user_id::", user_id);
+  console.log("user_pw::", user_pw);
   const userId = `select user_id from userTable where user_id='${user_id}'`;
   db.query(userId, (err, result) => {
     if (err) {
       console.log(`Error발생 시간::${new Date()}::${err}`);
     }
     console.log(result);
-    res.status(200).json({ userId: result });
+    let accessToken = generateAccessToken(user_id);
+    let refreshToken = generateRefreshToken(user_id);
+    res.status(200).json({ userId: result[0].user_id, ac: accessToken, rf:refreshToken });
   });
 });
 
