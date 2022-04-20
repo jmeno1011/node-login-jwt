03af5612f7a321fd4045dcbc97281ea77360f191
meno1011 <39692905+meno1011@users.noreply.github.com>
2022-04-20 17:43:16 +0900

    pool query 내용 변경

--- routers/pokemon.js
+++ routers/pokemon.js
@@ -5,11 +5,15 @@ const router = express.Router();
 
 router.get("/", (req, res) => {
   const userTable = "select * from pokemon";
-  pool.query(userTable, (err, result) => {
-    if (err) {
-      console.log(`Error발생 시간::${new Date()}::${err}`);
-    }
-    res.status(200).json({ pokemon: result });
+  pool.getConnection((err, connection)=>{
+    if(err) throw err;
+    connection.query(userTable, (err, result) => {
+      if (err) {
+        console.log(`Error발생 시간::${new Date()}::${err}`);
+      }
+      res.status(200).json({ pokemon: result });
+    });
+    connection.release();
   });
 });
 
